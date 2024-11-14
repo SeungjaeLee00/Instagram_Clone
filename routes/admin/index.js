b6201b523d9f6819e89edd2ceacd4c53dafcad57
leejaejae <leesengjie@naver.com>
2024-11-14 16:32:07 +0900

    feat: 관리자 유저 삭제 API 구현
    - 관리자는 일반 유저를 삭제할 수 있는 API를 구현함
    - 유저 삭제 시 해당 유저의 게시물, 댓글, 좋아요 정보도 함께 삭제됨
    - 관리자 권한 확인 로직 추가

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const adminSignupRoutes = require("./adminSignup");
+const adminUserRoutes = require("./adminDelelteUser");
+const adminCommentRoutes = require("./adminDeleteComment");
+const adminPostRoutes = require("./admInDeletePost");
 
 router.use("/adminSignup", adminSignupRoutes);
+router.use("/adminDelete/users", adminUserRoutes);
+// router.use("/adminDelete/comments", adminCommentRoutes);
+// router.use("/adminDelete/posts", adminPostRoutes);
 
 module.exports = router;
