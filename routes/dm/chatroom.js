4aef89eb700626a01f90a344caf164544e5c9c53
zIswim <enphkosta@naver.com>
2024-11-16 22:26:55 +0900

    채팅방 생성 수정

--- routes/dm/chatroom.js
+++ routes/dm/chatroom.js
@@ -6,6 +6,7 @@ const {auth} = require("../auth");
 
 const {Chatroom} = require("../../models/Chatroom");
 const cookieParser = require("cookie-parser");
+const { User } = require("../../models/User");
 router.use(cookieParser());
 
 // 내가 속해 있는 모든 채팅방 가져오기
@@ -37,31 +38,61 @@ router.get("/", auth, (req, res) => {
 
 // 채팅방이 없는 경우 만들기
 // :chatId => 채팅하고 싶은 상대의 _id
-router.post("/:chatId", auth, (res, req) => {
+router.post("/:chatId", auth, async(req, res) => {
     const userId = req.user._id;
-    const chatId = req.params._id;
+    const chatId = req.params.chatId;
 
-    const chatroom = new Chatroom({
-        members: [userId, chatId],
-        date: new Date(),
-        title: title
-    })
+    const { title } = req.body;
 
-    chatroom.save()
-    .then((result) => {
-        console.log({
+    try{
+        const targetId = await User.findOne({ user_id: chatId});
+        if(!targetId) {
+            return res.status(404)
+            .json({
+                error: "채팅 상대를 찾을 수 없습니다."
+            })
+        }
+
+        // 기존 채팅방 확인
+        const existChatroom = await Chatroom.findOne({
+            members: {$all: [userId, targetId]}
+        })
+
+        if(existChatroom){
+            return res.status(200).json({
+                message: "이미 채팅방이 있습니다.",
+                chatroomId: existChatroom._id,
+                title: existChatroom.title
+            });
+        }
+
+        const savedata = {
+            members: [userId, targetId],
+            date: new Date(),
+        }
+
+        // 타이틀이 있는경우
+        if(title) {
+            savedata.title = title
+        }
+
+        const chatroom = new Chatroom(savedata)
+        const result = await chatroom.save();
+
+        res.status(201).json({
             message: "채팅방 저장 완료",
-            title: result.title,
-            members : result.members,
-            Date : result.date
+            title: result.title || null,
+            members: result.members,
+            date: result.date,
         });
-    })
-    .catch((error) => {
+    }
+    catch(error){
+        console.log(error);
         return res.status(500).json({ 
             error: "채팅방 저장 실패",
             Message: error 
         });
-    });
+    }
 })
 
 module.exports = router ;
