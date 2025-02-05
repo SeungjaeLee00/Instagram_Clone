7859c466c03b99c114e5fd310ce3903607a0fb2f
leejaejae <leesengjie@naver.com>
2025-02-05 22:34:41 +0900

    fixed: 모델 구조 수정

--- models/Chatroom.js
+++ models/Chatroom.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 
-const chatroomSchema = new mongoose.Schema ({
-    title: {
-        type:String,
-        default: "채팅방" // 기본 채팅방
+const chatroomSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    default: "채팅방", // 기본 채팅방
+  },
+
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
+  ],
 
-    members : [{
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'User',
-        required: true,
-    }],
-
-    date: {
-        type: Date,
-        Default: Date.now
-    }
-
+  date: {
+    type: Date,
+    Default: Date.now,
+  },
 });
 
-const Chatroom = mongoose.model("Chatroom", chatroomSchema); 
+const Chatroom = mongoose.model("Chatroom", chatroomSchema);
 
-module.exports = {Chatroom};
+module.exports = { Chatroom };
